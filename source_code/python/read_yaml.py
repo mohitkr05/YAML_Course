6de2129b0f93cc29d9051266e8a3ee11d3900793
Mohit Kumar <mohitkr05@gmail.com>
2023-01-09 21:10:46 +1100

    Deployed 2533dab with MkDocs version: 1.4.2

---
+++ source_code/python/read_yaml.py
@@ -0,0 +1,31 @@
+import yaml
+import sys
+
+def check_file_supplied():
+    """
+    Function to check if the argument is supplied i.e. the filename is provided.
+    """
+    if len(sys.argv) != 2:
+        raise ValueError('Please provide YAML file as an argument')
+    yaml_file = sys.argv[1]
+    return yaml_file
+
+
+def print_yaml_file_contents(yaml_file):
+    """
+    Function to print YAML file contents
+    """
+    with open(yaml_file , 'r') as file:
+        yaml_file_content = yaml.safe_load(file)
+    
+    for key, values in yaml_file_content.items():
+        print(f'{key} ==> {values} ==> {type(values)} ')
+
+
+
+if __name__=="__main__":
+    """Calling the functions"""
+    yaml_file = check_file_supplied()
+    print_yaml_file_contents(yaml_file)
+
+    
